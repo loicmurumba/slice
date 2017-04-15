aed784bb78d550a083d6dad5d926530e44ba30b4
loicmurumba <muruloic@gmail.com>
2017-04-15 15:05:36 -0400

    more stuff

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var gameport = 4004;
 
-var		io = require('socket.io'),
+var		socket = require('socket.io'),
 		express = require('express'),
         UUID = require('uuid'),
         verbose  = false,
         app = express();
 
 server = app.listen( gameport );
-var socket = io(server);
+var io = socket(server);
 
-socket.sockets.on('connection', connectFunction)
 
 function connectFunction(socket){
 	console.log('we got hit');
-
-
 }
+
+io.sockets.on('connection', connectFunction)
+
         //Log something so we know that it succeeded.
 console.log('\t :: Express :: Listening on port ' + gameport );
 
